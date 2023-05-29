05f5e4b27e63ec1f47c074003b6d5e2c2b1a632b
nocarend <gudvin0203@gmail.com>
2023-05-29 12:52:57 +0300

    add test class

---
+++ example/src/test/java/org/example/MainTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void mainTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void kekTest() {
+        assertFalse(true);
+    }
+}
